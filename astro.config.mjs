ae1bd5ef01d11bde61668abf3b052b5be235e4df
Alfonso Reyes <vaca.hace.muu@gmail.com>
2023-10-07 18:27:55 -0700

    feat: upload site

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), sitemap(), tailwind(), image()],
+  site: "https://ojoanalogo.com",
+  compressHTML: true,
+});
---
+++ src/utils/dynamicImport.ts
@@ -0,0 +1,50 @@
+import path from "path";
+
+export function getFeaturedImage(fileName: string) {
+  const filename = path.parse(fileName);
+  const name = filename.name;
+  const ext = filename.ext;
+  switch (ext) {
+    case ".webp":
+      return import(`../assets/featured/${name}.webp`);
+    case ".jpg":
+      return import(`../assets/featured/${name}.jpg`);
+    case ".png":
+      return import(`../assets/featured/${name}.png`);
+    case ".svg":
+      return import(`../assets/featured/${name}.svg`);
+    case ".gif":
+      return import(`../assets/featured/${name}.gif`);
+    case ".avif":
+      return import(`../assets/featured/${name}.avif`);
+    case ".jpeg":
+      return import(`../assets/featured/${name}.jpeg`);
+    default:
+      return import(`../assets/featured/${name}.jpg`);
+  }
+}
+
+export function getBlogImage(slug: string, fileName: string) {
+  const filename = path.parse(fileName);
+  const name = filename.name;
+  const ext = filename.ext;
+
+  switch (ext) {
+    case ".webp":
+      return import(`../content/blog/${slug}/${name}.webp`);
+    case ".jpg":
+      return import(`../content/blog/${slug}/${name}.jpg`);
+    case ".png":
+      return import(`../content/blog/${slug}/${name}.png`);
+    case ".svg":
+      return import(`../content/blog/${slug}/${name}.svg`);
+    case ".gif":
+      return import(`../content/blog/${slug}/${name}.gif`);
+    case ".avif":
+      return import(`../content/blog/${slug}/${name}.avif`);
+    case ".jpeg":
+      return import(`../content/blog/${slug}/${name}.jpeg`);
+    default:
+      return import(`../content/blog/${slug}/${name}.jpg`);
+  }
+}
