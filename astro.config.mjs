2fa1b6cdfd8c024ae0cab683bfaa7015bbea01b0
Alfonso Reyes <vaca.hace.muu@gmail.com>
2023-10-15 15:13:05 -0700

    feat: rollback astro to 2.0, improve collections view design

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), image()],
   site: "https://ojoanalogo.com",
   compressHTML: true,
 });
--- src/content/collections/osaka-2023/index.md
+++ src/content/collections/osaka-2023/index.md
@@ -2,6 +2,6 @@
 title: "Osaka"
 description: "Hehe"
 publishDate: "September 19 2023"
-coverImage: "20231004_1_Pantera_Portra800_09"
+coverImage: "20231004_1_Pantera_Portra800_06"
 published: true
 ---
