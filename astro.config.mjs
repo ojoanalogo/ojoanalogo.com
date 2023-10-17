9e06af089862dc0b8f47e1d5e1a98fbe0786ce19
Alfonso Reyes <vaca.hace.muu@gmail.com>
2023-10-17 10:54:30 -0700

    feat: finish upgrade to 3.0, fix image sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()],
   site: "https://ojoanalogo.com",
   compressHTML: true,
-  image: {
-    service: squooshImageService(),
-  },
 });
